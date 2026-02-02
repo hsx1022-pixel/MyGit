33a9df21f6935e87658bbf24077185fbfe8774b9
ST-015\user <hsx1022@apps.ntpc.edu.tw>
2026-02-02 10:44:00 +0800

    hff1022

--- 3uyutduytd7duiyirxirs.py
+++ 3uyutduytd7duiyirxirs.py
@@ -1,1 +1,7 @@
-ovbbvvjuvb 
+def mySub(para1, para2):
+    print("statements to be executed")
+mySub(para1, para2)
+def myFunc(para1, para2):
+    print("statements to be executed")
+    return para1 + para2
+result = myFunc(para1, para2)
