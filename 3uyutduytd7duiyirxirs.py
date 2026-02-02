962bfd6958b30830d253d9be5d85aae576c6c21c
ST-015\user <hsx1022@apps.ntpc.edu.tw>
2026-02-02 10:47:38 +0800

    heiio world

--- 3uyutduytd7duiyirxirs.py
+++ 3uyutduytd7duiyirxirs.py
@@ -1,7 +1,3 @@
-def mySub(para1, para2):
-    print("statements to be executed")
-mySub(para1, para2)
-def myFunc(para1, para2):
-    print("statements to be executed")
-    return para1 + para2
-result = myFunc(para1, para2)
+import streamlit as st
+
+st.title("Hello worid!")
